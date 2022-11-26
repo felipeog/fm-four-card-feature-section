39e045f8c4f5fb4736f65c9f48cc7a75fb0eaa44
felipeog <17603069+felipeog@users.noreply.github.com>
2022-11-26 12:01:08 -0300

    Add content

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
-const TEMPLATE_ID = "feature-card-template";
 const SHADOW_CONFIG = { mode: "open" };
+const TEMPLATE_ID = "feature-card-template";
 
 class FeatureCard extends HTMLElement {
   constructor() {
     super();
 
+    const shadow = this.attachShadow(SHADOW_CONFIG);
     const template = document.getElementById(TEMPLATE_ID);
 
-    this.attachShadow(SHADOW_CONFIG).appendChild(
-      template.content.cloneNode(true)
-    );
+    shadow.appendChild(template.content.cloneNode(true));
+
+    const article = shadow.querySelector("article");
+
+    if (this.hasAttribute("accentColor")) {
+      const accentColor = this.getAttribute("accentColor");
+
+      article.style.borderTopColor = accentColor;
+    }
+
+    if (this.hasAttribute("iconHref")) {
+      const iconHref = this.getAttribute("iconHref");
+      const icon = new Image();
+
+      icon.src = iconHref;
+      article.appendChild(icon);
+    }
   }
 }
 
