630b1335e6e69ab8a6dd142c06b84dc3639d1ddb
felipeog <17603069+felipeog@users.noreply.github.com>
2022-11-27 18:44:32 -0300

    Create text component

---
+++ js/components/Text.js
@@ -0,0 +1,61 @@
+class Text extends HTMLElement {
+  static elementName = "custom-text";
+  static elementAttributes = {
+    size: "size",
+    text: "text",
+  };
+
+  constructor() {
+    super();
+
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.setStyles();
+    this.render();
+  }
+
+  setStyles() {
+    const style = document.createElement("style");
+
+    style.textContent = `
+      p {
+        color: hsl(229, 6%, 66%);
+        padding: 0;
+        margin: 0;
+      }
+
+      .sm {
+        font-size: 0.8rem;
+      }
+
+      .md {
+        font-size: 1rem;
+      }
+
+      .lg {
+        font-size: 1.2rem;
+      }
+    `;
+    this.shadow.appendChild(style);
+  }
+
+  render() {
+    const paragraph = document.createElement("p");
+
+    if (this.hasAttribute(Text.elementAttributes.text)) {
+      const text = this.getAttribute(Text.elementAttributes.text);
+      const textTextNode = document.createTextNode(text);
+
+      paragraph.appendChild(textTextNode);
+    }
+
+    if (this.hasAttribute(Text.elementAttributes.size)) {
+      const size = this.getAttribute(Text.elementAttributes.size);
+
+      paragraph.classList.add(size);
+    }
+
+    this.shadow.appendChild(paragraph);
+  }
+}
+
+export { Text };
--- js/components/index.js
+++ js/components/index.js
@@ -1,5 +1,6 @@
 import { FeatureCard } from "./FeatureCard.js";
 import { Heading } from "./Heading.js";
 import { Icon } from "./Icon.js";
+import { Text } from "./Text.js";
 
-export const components = [FeatureCard, Heading, Icon];
+export const components = [FeatureCard, Heading, Icon, Text];
